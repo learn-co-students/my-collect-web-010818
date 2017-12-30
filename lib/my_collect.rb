44e38afd926bf72121f13626a33623356bc3b430
Folu Obatusin <nisutabo@gmail.com>
2017-12-30 22:45:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(a)
+  if block_given?
+    b = []
+    i = 0
+    while i < a.length
+      m = yield a[i]
+      b << m
+      i += 1
+    end
+    b
+  else
+    "Hey! No block was given!"
+  end
+end
